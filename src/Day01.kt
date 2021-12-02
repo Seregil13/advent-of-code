95bc223d46065ca4ac1f6a988cbef5d6cb8919e4
seregil13 <alecerietman@gmail.com>
2021-12-01 20:01:47 -0800

    upgrade day 1 with cleaner std library functions like zip and windowed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,39 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input
-            .filterIndexed { index, s ->
-                if (index == 0) return@filterIndexed false
-
-                val current = s.toInt()
-                val previous = input[index - 1].toInt()
-
-                current > previous
-            }
-            .count()
+        return input.map { it.toInt() }
+            .zipWithNext()
+            .count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
-
-        val x: List<Int> = input
-            .dropLast(2)
-            .mapIndexed { index, s ->
-                s.toInt() + input[index + 1].toInt() + input[index + 2].toInt()
-            }
-
-        return x
-            .filterIndexed { index, i ->
-                if (index == 0) return@filterIndexed false
-
-                val previous = x[index - 1]
-
-                i > previous
-            }
-            .count()
+        return input.asSequence().map { it.toInt() }
+            .windowed(3).map { it.sum() }
+            .zipWithNext()
+            .count { (a, b) -> b > a }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
